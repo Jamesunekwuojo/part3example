03871c59b4df8e5f5a1fe867e8b437bc9145750c
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-11-12 13:29:23 +0100

    folder restructured

--- phonebook/server/index.js
+++ phonebook/server/index.js
@@ -1,12 +1,31 @@
-console.log('hello world')
+// console.log('hello world')
 
 
 // Created a simple web server, using the http module
 const http = require('http')
 
+
+let notes = [
+    {
+      id: "1",
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: "2",
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: "3",
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+  response.end(JSON.stringify(notes))
 })
 
 const PORT = 3001
